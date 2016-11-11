1bac3380e07abb252b5fb200edf3900705afd860
jescriba <escribirajoshua@gmail.com>
2016-11-10 20:12:47 -0800

    Adds custom view

--- CardsViewController.swift
+++ CardsViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
-    @IBOutlet weak var draggableImageView: DraggableImageView!
+    var draggableImageView: DraggableImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        draggableImageView = Bundle.main.loadNibNamed("DraggableImageView", owner: self, options: nil) as! DraggableImageView
-        add
-        view.addSubView(draggableImageView)
+        draggableImageView = DraggableImageView(frame: CGRect(x: 0, y: 100, width: 304, height: 304))
+        view.addSubview(draggableImageView)
         // Do any additional setup after loading the view.
     }
 }
