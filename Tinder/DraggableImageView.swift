c8286190d84a2f0adc4b208d5f325a9e3ca783c6
jescriba <escribirajoshua@gmail.com>
2016-11-10 20:32:24 -0800

    Adds swiping the card off

--- Tinder/DraggableImageView.swift
+++ Tinder/DraggableImageView.swift
@@ -32,11 +32,40 @@ class DraggableImageView: UIView {
 
     var originalCenter:CGPoint?
     @IBAction func onDrag(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: contentView).x
         if sender.state == .began {
             originalCenter = sender.view?.center
         } else if sender.state == .changed {
-            sender.view?.center.x =
-                sender.translation(in: contentView).x + (originalCenter?.x)!
+            sender.view?.center.x = translation + (originalCenter?.x)!
+            let angle = (sender.view?.center.x)!.degreesToRadians / 20
+            if translation < 0 {
+                sender.view?.transform = CGAffineTransform(rotationAngle: -1 * angle)
+            } else if translation > 0 {
+                sender.view?.transform = CGAffineTransform(rotationAngle: 1 * angle )
+            }
+        } else {
+            if translation < -50 {
+                UIView.animate(withDuration: 0.4, animations: {
+                    sender.view?.center.x = -1 * self.contentView.frame.width
+                })
+            } else if translation > 50 {
+                UIView.animate(withDuration: 0.4, animations: {
+                    sender.view?.center.x = self.originalCenter!.x + self.contentView.frame.width
+                })
+            } else {
+                sender.view?.transform = .identity
+                sender.view?.center.x = originalCenter!.x
+            }
         }
     }
+}
+
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi / 180 }
+    var radiansToDegrees: Double { return Double(self) * 180 / .pi }
+}
+extension FloatingPoint {
+    var degreesToRadians: Self { return self * .pi / 180 }
+    var radiansToDegrees: Self { return self * 180 / .pi }
 }
