d718caf95a2668690d90558d48c5101a518a000a
jescriba <escribirajoshua@gmail.com>
2016-11-10 20:48:31 -0800

    Adds detail view controller

--- CardsViewController.swift
+++ CardsViewController.swift
@@ -14,9 +14,19 @@ class CardsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let tapper = UITapGestureRecognizer(target: self, action: #selector(CardsViewController.onTap))
+        
+        
+        
         draggableImageView = DraggableImageView(frame: CGRect(x: 0, y: 100, width: 304, height: 304))
+        draggableImageView.addGestureRecognizer(tapper)
         view.addSubview(draggableImageView)
         // Do any additional setup after loading the view.
+    }
+    
+    func onTap(_ sender: UITapGestureRecognizer) {
+        let profile = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
+        
+        present(profile, animated: true, completion: nil)
     }
 }
